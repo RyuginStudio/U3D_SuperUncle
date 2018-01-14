ee695080bf845b083a83fa0a461546e3680110eb
vszed <zyrpg@vip.qq.com>
2018-01-15 01:39:02 +0800

    mapCreator

---
+++ Scripts/CharacterControler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControler : MonoBehaviour
+{
+
+    public float speed = 1;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        keyboardControl();
+    }
+
+    void keyboardControl()
+    {
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    var pos = transform.position;
+        //    this.transform.position = Vector3.MoveTowards(pos, new Vector3(pos.x - 10, pos.y, pos.z), speed * Time.deltaTime);
+        //}
+        //else if(Input.GetKey(KeyCode.D))
+        //{
+        //    var pos = transform.position;
+        //    this.transform.position = Vector3.MoveTowards(pos, new Vector3(pos.x + 10, pos.y, pos.z), speed * Time.deltaTime);
+        //}
+
+        var horizontal = Input.GetAxis("Horizontal");
+        var verticle = Input.GetAxis("Vertical");
+
+        Debug.Log(horizontal);
+        Debug.Log(verticle);
+
+        transform.Translate(new Vector2(horizontal * 0.1f, verticle * 0.1f), Space.World);
+
+    }
+}
---
+++ Scripts/MapEditor/MapEditor.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * 时间：2018年1月14日22:32:53
+ * 作者：vszed
+ * 功能：地图编辑器
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MapEditor : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        drawMap();
+    }
+
+    public Transform mapEditor;
+
+    public Toggle[] ToggleMapBlock;      //地图图块Toggle
+    public GameObject[] PrefabMapBlock;  //地图图块Prefab
+
+
+    public int getSingleBlockChoice()  //获取单一图块选择“索引值”
+    {
+        int idx = 0;
+
+        while (idx < ToggleMapBlock.Length)
+        {
+            if (ToggleMapBlock[idx].isOn)
+                break;
+            idx++;
+        }
+        return idx;
+    }
+
+    public void drawMap()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Debug.Log(getSingleBlockChoice());
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);  //鼠标的屏幕坐标转成世界坐标
+            Instantiate(PrefabMapBlock[getSingleBlockChoice()], new Vector3(pos.x, pos.y, 0), new Quaternion(), mapEditor);
+        }
+    }
+}
