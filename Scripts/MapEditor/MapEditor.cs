ca49337cad1e91f08b9da35a10e129d4fe38ff4a
vszed <zyrpg@vip.qq.com>
2018-01-17 01:29:52 +0800

    MapEditor

--- Scripts/MapEditor/MapEditor.cs
+++ Scripts/MapEditor/MapEditor.cs
@@ -12,23 +12,37 @@ using UnityEngine.EventSystems;
 
 public class MapEditor : MonoBehaviour
 {
+    private static MapEditor instance;
+
+    public static MapEditor getInstance()
+    {
+        return instance;
+    }
 
     // Use this for initialization
     void Start()
     {
+        instance = this;
 
+        createMask(16, 32);// row, column);
     }
 
     // Update is called once per frame
     void Update()
     {
-        drawMap();
+
     }
 
     public Transform mapEditor;
+    public Transform mapMaskGrid;
+
+    public GameObject PrefabMask;        //图块遮罩
 
     public Toggle[] ToggleMapBlock;      //地图图块Toggle
     public GameObject[] PrefabMapBlock;  //地图图块Prefab
+
+    public int row;     //行
+    public int column;  //列
 
 
     public int getSingleBlockChoice()  //获取单一图块选择“索引值”
@@ -44,13 +58,23 @@ public class MapEditor : MonoBehaviour
         return idx;
     }
 
-    public void drawMap()
+    public void drawBlock(Vector3 pos)
     {
-        if (Input.GetKey(KeyCode.Mouse0) && !EventSystem.current.IsPointerOverGameObject())
+        Debug.Log("Pos: " + Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        Instantiate(PrefabMapBlock[getSingleBlockChoice()], new Vector3(pos.x, pos.y, 0), new Quaternion(), mapEditor);
+    }
+
+    public void createMask(int row, int column)  //创建网格遮罩
+    {
+        for (int i = 0; i < column; i++)
         {
-            Debug.Log(getSingleBlockChoice());
-            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);  //鼠标的屏幕坐标转成世界坐标
-            Instantiate(PrefabMapBlock[getSingleBlockChoice()], new Vector3(pos.x, pos.y, 0), new Quaternion(), mapEditor);
+            for (int j = 0; j < row; j++)
+            {
+                Instantiate(PrefabMask, new Vector3(0.64f * i, 0.64f * j, 0), new Quaternion(), mapMaskGrid);
+            }
         }
+
+        //坐标写死，将坐标系移至左下角
+        mapMaskGrid.transform.position = new Vector3(-8.574f, -3.683f, 0);
     }
 }
