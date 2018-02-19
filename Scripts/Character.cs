c2c31c591b4da3ba036af6e2e2d37407f3019309
vszed <zyrpg@vip.qq.com>
2018-02-19 16:29:11 +0800

    idle animation

--- Scripts/Character.cs
+++ Scripts/Character.cs
@@ -14,22 +14,71 @@ public class Character : MonoBehaviour
     {
         idle,
         move,
+        run,
         jump,
     }
 
     public Status characStatus;
 
+    //角色朝向
+    public enum Direction
+    {
+        left,
+        right
+    }
+
+    public Direction characDirection;
+
+    public void changeStatus(float horizontal)  //状态切换
+    {
+        if (horizontal != 0)
+        {
+            characStatus = Status.move;
+            Animator anim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+            anim.SetBool("isRun", true);
+            anim.SetBool("isIdle", false);
+        }
+        else if (!Input.GetKey(KeyCode.J) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            characStatus = Status.idle;
+            Animator anim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isRun", false);
+        }
+    }
+
+    public void changeDirection(float horizontal)
+    {
+        if (horizontal < 0 && characDirection != Direction.left)
+        {
+            characDirection = Direction.left;
+            GameObject.FindGameObjectWithTag("Player").transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+
+        if (horizontal > 0 && characDirection != Direction.right)
+        {
+            characDirection = Direction.right;
+            GameObject.FindGameObjectWithTag("Player").transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+    }
+
+    //角色状态初始化
+    void characInit()
+    {
+        characDirection = Direction.right;
+        characStatus = Status.idle;
+    }
+
     // Use this for initialization
     void Start()
     {
-
+        characInit();
     }
 
     // Update is called once per frame
     void Update()
     {
         keyboardControl();
-        rotateAnim(Input.GetAxis("Horizontal"));
     }
 
     void keyboardControl()
@@ -37,17 +86,13 @@ public class Character : MonoBehaviour
         var horizontal = Input.GetAxis("Horizontal");
         //var verticle = Input.GetAxis("Vertical");
 
-        //Debug.Log(horizontal);
-        //Debug.Log(verticle);
-
         GetComponent<Rigidbody2D>().AddForce(new Vector2(horizontal * MoveSpeed, 0));
+
+        //切换动画状态
+        changeStatus(horizontal);
+
+        //切换角色朝向
+        changeDirection(horizontal);
     }
 
-    void rotateAnim(float horizontal)  //角色转向动画
-    {
-        if(horizontal != 0)
-        {
-            
-        }
-    }
 }
