f8d3156afa7945a7ea492978f78e9b3515fdf0b8
vszed <zyrpg@vip.qq.com>
2018-02-28 00:26:44 +0800

    MessageBox, cover namesake map or not
    1.cover namesake map or not, will show messageBox
    2.messageBox has confirm and cancel btn, 为messageBox设置了预制体，具体按钮的onClick事件，通过delegate委托机制动态赋予
    btn.onClick.addListener(delegate()
    {
         //具体执行的内容或者函数
    });

--- Scripts/MapEditor/ReadCreateMap.cs
+++ Scripts/MapEditor/ReadCreateMap.cs
@@ -47,12 +47,50 @@ public class ReadCreateMap : MonoBehaviour
         string path = Application.dataPath + "//Resources/MapConfig/" + inputFiledName.text + ".json";
 
         //检查是否存在该名字的地图
-        if(IsFileExists(Application.dataPath + "//Resources/MapConfig/" + inputFiledName.text + ".json"))
+        if (IsFileExists(Application.dataPath + "//Resources/MapConfig/" + inputFiledName.text + ".json"))
         {
             //TODO:需要messagebox是否覆盖
+            var MessageBox = Instantiate(Resources.Load("Prefab/MessageBox"), GameObject.Find("Canvas").transform);
+
+            ((GameObject)MessageBox).GetComponentInChildren<Text>().text = "您所设定的地图名称与本地地图库中现有地图的名称发生重复！是否要保存新地图从而覆盖掉旧地图？？？";
+
+            var btnPack = ((GameObject)MessageBox).GetComponentsInChildren<Button>();
+
+            foreach (var btn in btnPack)
+            {
+                if (btn.name == "ConfirmBtn")
+                {
+                    btn.onClick.AddListener(delegate ()       //委托：http://blog.csdn.net/cocos2der/article/details/42705885
+                    {
+                        doCreateMap(path);
+
+                        ((GameObject)MessageBox).GetComponentInChildren<Text>().text = "新地图已完成覆盖！！！";
+
+                        foreach (var item in btnPack)
+                        {
+                            GameObject.Destroy(item);
+                        }
+
+                        GameObject.Destroy(MessageBox, 2);
+                    });
+                }
+                else if (btn.name == "CancelBtn")
+                {
+                    btn.onClick.AddListener(delegate ()
+                    {
+                        GameObject.Destroy(MessageBox);
+                    });
+                }
+            }
         }
+        else
+        {
+            doCreateMap(path);
+        }
+    }
 
-
+    private void doCreateMap(string path)
+    {
         StringBuilder sb = new StringBuilder();
         JsonWriter writer = new JsonWriter(sb);
 
