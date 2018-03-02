1bc8c4292f4476e269658337b9ad113d79b0d77b
vszed <zyrpg@vip.qq.com>
2018-03-02 19:41:23 +0800

    GoomBa step 1

---
+++ Scripts/Goomba.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * 时间：2018年3月2日18:47:31
+ * 作者：vszed
+ * 功能：板栗 最初级敌人
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goomba : MonoBehaviour
+{
+    private float currentTime;
+    private float directionUpdate;
+
+    //0.1秒之前的坐标
+    private Vector3 previousPos;
+
+    public float moveSpeed = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentTime = Time.time;
+        previousPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = Time.time;
+
+        changeDirection();
+        Move();
+    }
+
+    public enum direction
+    {
+        left,
+        right
+    }
+    public direction GoombaDirection;
+
+    //移动
+    public void Move()
+    {
+        if (GoombaDirection == direction.right)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+    }
+
+    //转向 => (0.1秒前的坐标与当前坐标相同时视为障碍物)
+    public void changeDirection()
+    {
+        if (currentTime - directionUpdate > 0.1f)
+        {
+            if (previousPos == transform.position)
+                GoombaDirection = GoombaDirection == direction.right ? direction.left : direction.right;
+
+            previousPos = transform.position;
+            directionUpdate = Time.time;
+        }
+    }
+}
