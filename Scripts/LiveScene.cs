219a88a6fd724f944d4932e80f168f225da24ca6
vszed <zyrpg@vip.qq.com>
2018-03-23 15:07:13 +0800

    LiveScene

---
+++ Scripts/LiveScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LiveScene : MonoBehaviour
+{
+    public Text livesNum;
+
+    // Use this for initialization
+    void Start()
+    {
+        livesNum.text = GameData.MarioLives.ToString();
+        Invoke("jumpScene",3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void jumpScene()
+    {
+        StartCoroutine(SceneTransition.getInstance().loadScene("MainScene", 2));
+    }
+}
